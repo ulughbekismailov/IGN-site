f46a1ba11c338fa2831a7cd4fa9d8175783aa58a
UI <xmail@kali.xmail>
2026-04-03 15:30:29 +0500

    two commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 export default defineConfig({
   plugins: [vue()],
+  base: '/',
   resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } }
 })
